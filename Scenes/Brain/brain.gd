de9a607e677677853c56a9a98eedbd4839c4157f
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-09 13:39:35 +0200

    fix intro and movement scrtipt

--- Scenes/Brain/brain.gd
+++ Scenes/Brain/brain.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const STOP_SPEED = 2500.0
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.y = direction.y * SPEED
+
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		#velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, STOP_SPEED * delta)
+
 	move_and_slide()
