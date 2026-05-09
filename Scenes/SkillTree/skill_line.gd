673ad3cda0e3de76a10ad3e3b77abe04ee9c7c22
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-10 00:22:48 +0200

    lef tside skill tree visuals

---
+++ Scenes/SkillTree/skill_line.gd
@@ -0,0 +1,13 @@
+extends Line2D
+
+@export var skill_to: String = "fast_bullets"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if SkillDatabase.skills_map[skill_to].pre_skill_requirement_satisfied():
+		width = 10
--- Scenes/SkillTree/skill_tree.gd
+++ Scenes/SkillTree/skill_tree.gd
@@ -4,8 +4,7 @@ signal restart_run
 
 const SKILL_NODE = preload("res://Scenes/SkillTree/skill_node.tscn")
 
-@onready var lines = $Lines
-@onready var nodes = $Nodes
+
 
 func _on_restart_pressed():
 	print("RESTART CLICKED")
