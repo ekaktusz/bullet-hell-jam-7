9ca20d11b5e8126daa469636acda948da3097002
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-10 22:31:24 +0200

    asda

--- Scenes/Map/ui_canvas_layer.gd
+++ Scenes/Map/ui_canvas_layer.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 @onready var map: Node2D = $".."
 @onready var time_label: Label = $TimeLabel
-@onready var current_money_label: Label = $CurrentMoneyLabel
 @onready var bullets_label: Label = $BulletsLabel
 @onready var hp_1: Sprite2D = $Health/hp1
 @onready var hp_2: Sprite2D = $Health/hp2
@@ -17,7 +16,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_label.text = "TIME: " + str(int(map.run_time * SkillDatabase.reward_multiplier))
-	current_money_label.text = "current_money: " + str(CommonGlobals.current_money)
 	bullets_label.text = str(map.bullet_manager.bullets.size()) + " / " + str(SkillDatabase.max_bullet_count)
 
 func _on_player_hit():
