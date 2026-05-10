923145e039150d2e372c088875ed12551fe2a02b
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-10 18:24:50 +0200

    keep last mouse location

--- Scenes/Brain/brain.gd
+++ Scenes/Brain/brain.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 const STOP_SPEED = 2500.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var last_mouse_position := Vector2.ZERO
+
 func _ready() -> void:
 	SkillDatabase.player_size_skill_emitter.connect(_on_player_size_level_up)
 	GameEvents.hit_player_emitter.connect(_on_hit_player)
 	GameEvents.shoot.connect(_on_shoot)
-	
+	last_mouse_position = get_global_mouse_position()
+
 const AIM_DEADZONE = 0.3
 
 func _physics_process(delta: float) -> void:
@@ -18,11 +21,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, STOP_SPEED * delta)
 
+	var current_mouse_position := get_global_mouse_position()
 	var aim_vector := Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_X), Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y))
 	if aim_vector.length() > AIM_DEADZONE:
 		rotation = aim_vector.angle() + PI / 2
-	else:
-		rotation = (get_global_mouse_position() - global_position).angle() + PI / 2
+		last_mouse_position = current_mouse_position
+	elif current_mouse_position != last_mouse_position:
+		rotation = (current_mouse_position - global_position).angle() + PI / 2
+		last_mouse_position = current_mouse_position
 	move_and_slide()
 
 
