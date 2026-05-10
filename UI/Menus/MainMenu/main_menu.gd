0c41c9a8d08e4b6b6a70a409c354b9ee1f3a5feb
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-10 18:51:48 +0200

    focus on menu start

--- UI/Menus/MainMenu/main_menu.gd
+++ UI/Menus/MainMenu/main_menu.gd
@@ -2,5 +2,10 @@ extends Control
 
 const MAP_SCENE = preload("res://Scenes/Map/Map.tscn")
 
+@onready var start_button: Button = $StartButton
+
+func _ready() -> void:
+	start_button.grab_focus()
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(MAP_SCENE)
