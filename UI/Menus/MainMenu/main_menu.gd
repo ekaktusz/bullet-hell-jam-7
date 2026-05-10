b9fe20b2b52db015776723b06f006160458ada7d
Csaba Ekart <csaba.ekart@gmail.com>
2026-05-10 17:37:50 +0200

    fix menu

--- UI/Menus/MainMenu/main_menu.gd
+++ UI/Menus/MainMenu/main_menu.gd
@@ -1,9 +1,6 @@
 extends Control
 
-func _process(delta: float) -> void:
-	print("fing")
+const MAP_SCENE = preload("res://Scenes/Map/Map.tscn")
 
 func _on_start_button_pressed() -> void:
-	#await SoundManager.play_click_sound().finished
-	print("segg")
-	get_tree().change_scene_to_file("res://Scenes/Map/Map.tscn")
+	get_tree().change_scene_to_packed(MAP_SCENE)
